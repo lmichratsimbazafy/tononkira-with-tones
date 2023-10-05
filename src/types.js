78d7090981c752528eed48dfd7c302e6fb2101e9
lmichratsimbazafy <ratsimbazafylmich@gmail.com>
2023-10-05 11:54:06 +0200

    remove build files

--- src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RealmModelNames = void 0;
-var RealmModelNames;
-(function (RealmModelNames) {
-    RealmModelNames["Author"] = "Author";
-    RealmModelNames["Lyrics"] = "Lyrics";
-})(RealmModelNames = exports.RealmModelNames || (exports.RealmModelNames = {}));
