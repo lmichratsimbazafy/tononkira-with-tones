edd5c05b98bf9f0a78e679450b03fb7bb2d78297
lmichratsimbazafy <ratsimbazafylmich@gmail.com>
2023-10-10 22:47:55 +0200

    router update

--- src/components/navigation/MainRouter.tsx
+++ src/components/navigation/MainRouter.tsx
@@ -1,16 +1,59 @@
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {FC} from 'react';
 import AppUpdate from '../../screen/AppUpdate/AppUpdate';
-import HomeScreen from '../../screen/Home/HomeScreen';
+import DetailsScreen from '../../screen/Lyrics/DetailsScreen';
 import SearchScreen from '../../screen/Search/SearchScreen';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import colours from '../../config/colors';
-const mainRouter = [
-  {name: 'Search', component: SearchScreen},
-  {name: 'Update', component: AppUpdate},
+import Program from '../../screen/Program/Program';
+
+const searchRoute = [
+  {
+    name: 'SearchScreen',
+    component: SearchScreen,
+  },
+  {
+    name: 'Details',
+    component: DetailsScreen,
+  },
 ];
 
+const SearchNavigator = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      {searchRoute.map((router, index) => (
+        <Stack.Screen
+          {...{
+            ...router,
+            key: index,
+            options: {
+              headerTitle: '',
+              headerTransparent: true,
+              header: () => null,
+            },
+          }}
+        />
+      ))}
+    </Stack.Navigator>
+  );
+};
+
+const mainRouter = [
+  {
+    name: 'Search',
+    component: SearchNavigator,
+  },
+  {
+    name: 'Update',
+    component: AppUpdate,
+  },
+  {
+    name: 'Program',
+    component: Program,
+  },
+];
 export const NavigationProvider: FC = () => {
   const Drawer = createDrawerNavigator();
   return (
@@ -24,17 +67,7 @@ export const NavigationProvider: FC = () => {
               options: {
                 headerTitle: '',
                 headerTransparent: true,
-                header: ({navigation}) => {
-                  return (
-                    <Ionicons
-                      name="menu"
-                      size={26}
-                      style={{marginLeft: 10, padding: 10}}
-                      color={colours.secondaryGrey}
-                      onPress={() => navigation.openDrawer()}
-                    />
-                  );
-                },
+                header: () => null,
               },
             }}
           />
