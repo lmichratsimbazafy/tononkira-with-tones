8e5d074e274336444beee625d32d89d7a7ffab1f
lmichratsimbazafy <ratsimbazafylmich@gmail.com>
2023-10-10 22:53:23 +0200

    [DRAFT] init program screen

---
+++ src/screen/Program/Program.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import MainLayout from '../../components/layout/MainLayout';
+
+const Program = () => {
+  return (
+    <MainLayout>
+      <Text>Program</Text>
+    </MainLayout>
+  );
+};
+
+export default Program;
+
+const styles = StyleSheet.create({});
