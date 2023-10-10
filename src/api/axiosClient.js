5416c749bc08a34a30da1bc8d49e4f5becee9124
lmichratsimbazafy <ratsimbazafylmich@gmail.com>
2023-10-10 22:12:23 +0200

    package update

--- src/api/axiosClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.client = void 0;
-var axios_1 = require("axios");
-var react_native_config_1 = require("react-native-config");
-console.log('Config.API_URL', react_native_config_1["default"].API_URL);
-exports.client = axios_1["default"].create({
-    baseURL: react_native_config_1["default"].API_URL
-});
